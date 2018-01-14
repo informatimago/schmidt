a5ad678fe893edf793ce1350b4f592abcf61f632
Pascal J. Bourguignon <pjb@informatimago.com>
2018-01-14 19:08:10 +0100

    Added SchmidtSynthesizerLibrarian iOS project.

---
+++ sources/SchmidtSynthesizerLibrarian/SchmidtSynthesizerLibrarian/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SchmidtSynthesizerLibrarian
+//
+//  Created by Pascal Bourguignon on 14/01/2018.
+//  Copyright © 2018 Pascal Bourguignon. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
