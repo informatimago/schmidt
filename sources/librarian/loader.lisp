92cd853f77734739d685b6711018043bd2586b36
Pascal J. Bourguignon <pjb@informatimago.com>
2021-04-29 00:19:29 +0200

    Added paths to asdf:*central-registry*.

--- sources/librarian/loader.lisp
+++ sources/librarian/loader.lisp
@@ -1,8 +1,10 @@
 (cd #P"~/works/synth/schmidt/sources/librarian/")
 
-(push #P"~/works/synth/CoreMIDI/"                  asdf:*central-registry*)
-(push #P"~/works/synth/schmidt/sources/librarian/" asdf:*central-registry*)
+(pushnew #P"~/works/synth/CoreMIDI/"                  asdf:*central-registry* :test (function equalp))
+(pushnew #P"~/works/synth/midi/"                      asdf:*central-registry* :test (function equalp))
+(pushnew #P"~/works/synth/schmidt/sources/librarian/" asdf:*central-registry* :test (function equalp))
 
 (ql:quickload :com.informatimago.synthesizer.schmidt.librarian)
+
 (in-package :com.informatimago.synthesizer.schmidt)
 (in-package :com.informatimago.synthesizer.schmidt.librarian)
